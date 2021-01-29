892254b217124630ebe5ac92f52908c97eb273ed
Alireza-Zamani <alirezazamani.azo@gmail.com>
2021-01-29 14:55:18 +0330

    Lose condition and timer

---
+++ Lost Soul Found Soul/Assets/Scripts/LoseCondition.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LoseCondition : MonoBehaviour
+{
+
+    #region Veriables 
+
+    private static LoseCondition _instance;
+
+    public static event Action<float> OnTimerUpdate;
+
+    //game time in seconds.
+    [SerializeField] private float gameTime = 600;
+
+    [SerializeField] private List<GameObject> canvasesToDeactivate;
+
+    #endregion
+
+    #region Getter/Setter 
+
+    public float GameTime { get => gameTime; set => gameTime = value; }
+
+    #endregion
+
+    #region Inbuilt Functions 
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+
+        if (_instance != null && _instance != this)
+            Destroy(this.gameObject);
+        else
+            _instance = this;
+    }
+
+    void Start() => Debug.Log("Game Timer Started");
+
+    void Update() => UpdateGameTime();
+
+    #endregion
+
+    #region Functions 
+
+    public static LoseCondition Instance { get { return _instance; } }
+
+    private void UpdateGameTime()
+    {
+        GameTime = GameTime - Time.deltaTime;
+
+        if (OnTimerUpdate != null)
+            OnTimerUpdate(GameTime);
+
+        if (GameTime <= 0)
+            Lose();         
+    }
+
+    private void Lose()
+    {
+        foreach (var obj in canvasesToDeactivate)
+            obj.SetActive(false);
+
+
+        Cursor.lockState = CursorLockMode.None;
+
+        Time.timeScale = 0;
+
+        GameObject loseUI = GameObject.Find("LoseUI");
+
+        loseUI.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    #endregion
+
+}
---
+++ Lost Soul Found Soul/Assets/Scripts/TimerUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+
+    #region Varibales
+
+    public Text timerText;
+
+    private float _timer = 0;
+
+    #endregion
+
+    #region Inbuilt Functions
+
+    private void Start()
+    {
+        LoseCondition.OnTimerUpdate += TimerCatcher;
+    }
+
+    void Update()
+    {
+        int sec = Mathf.FloorToInt(_timer % 60);
+        int min = Mathf.FloorToInt(_timer / 60);
+
+        timerText.text = min.ToString() + ":" + sec.ToString(); 
+    }
+
+    #endregion
+
+    #region Functions 
+
+    private void TimerCatcher(float gameTime)
+    {
+        _timer = gameTime;
+    } 
+
+    #endregion
+
+}
